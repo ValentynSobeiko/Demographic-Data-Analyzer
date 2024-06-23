c76e82dabfe2da3e2b51856964054936653cbb9a
Valentyn Sobeiko <90830883+ValentynSobeiko@users.noreply.github.com>
2024-06-23 17:21:22 +0200

    Add files via upload

---
+++ the_string = 'Hello'.py
@@ -0,0 +1,9 @@
+my_list = [
+    ['Felix', 'Cook'],
+    ['Michael', 'Janitor'],
+    ['Peter', 'Teacher'],
+    ['Ella', 'Mechanic']
+]
+
+print(my_list[0])  # Output: ['Felix', 'Cook']
+print(my_list[1][1])  # Output: 'Janitor'
